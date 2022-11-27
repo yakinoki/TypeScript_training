509cd059c71da31b40cb0d1190ce8117c5229864
unknown <yakinoki>
2022-11-27 19:27:48 +0900

    update_a_code

--- training.ts
+++ training.ts
@@ -6,7 +6,28 @@ console.log(t && t);
 const user = {
     name: "John",
     age: 30,
+    isteen() {
+        return this.age >=13 && this.age <=19;
+    }
 };
 
 user.name = "Adam";
 console.log(user.name);
+console.log(user.isteen());
+user.age = 15
+console.log(user.isteen());
+
+function Hello(name: string):string {
+    return `Hello ${name}`
+}
+console.log(Hello('Paul'))
+
+
+type SupportVersions = {
+    [env: number]: boolean;
+}
+
+let versions: SupportVersions = {
+    102: true,
+    200: false,
+}
