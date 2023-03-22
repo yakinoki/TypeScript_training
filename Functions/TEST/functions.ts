5784c614fab3b367dd25a7c874eb6e7516b2bb9b
unknown <yakinoki>
2023-03-22 22:52:06 +0900

    update_code

--- Functions/TEST/functions.ts
+++ Functions/TEST/functions.ts
@@ -1,6 +1,11 @@
-export function testHello(name: string):string {
-    return `testHello ${name}`
+export function testHello(name: string, userid?: string):string {
+    const id = (userid === undefined?"000":userid);
+    console.log(id);
+    const id2 = userid || "000";
+    console.log(id2);
+    return `test hello ${name}`
 }
-console.log(testHello('Paul'))
+console.log(testHello('Paul', 'www'))
+
 
 
