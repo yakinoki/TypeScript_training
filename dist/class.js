8d53f191af37fb616fc59d237c2ab9ef54ae286f
NAOYA\bundl <yakinoki>
2025-04-27 19:04:08 +0900

    update gitignore

--- dist/class.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// クラスによって作成されたオブジェクトをインスタンスと呼ぶ。
-class Person {
-    constructor(initName) {
-        this.name = initName;
-    }
-}
-const michael = new Person('Michael');
-console.log(michael);
